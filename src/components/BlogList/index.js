ef2a55f6a49a02f33a08ee730792d1a59b39a277
machetimurali <muralimacheti15198@gmail.com>
2023-08-31 13:17:12 +0000

    blog-list-app-reactjs

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="bolg-list">
+      {blogsList.map(eachBlog => (
+        <BlogItem blogDetails={eachBlog} key={eachBlog.id} />
+      ))}
+    </ul>
+  )
+}
+export default BlogList
